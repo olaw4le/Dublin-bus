e14614e957208f0e7c46acbb89f4a29ae857c276
hbarrett100 <55139243+hbarrett100@users.noreply.github.com>
2020-06-09 17:27:13 +0100

    add templates

--- web_app/journeyplanner/views.py
+++ web_app/journeyplanner/views.py
@@ -1,15 +1,39 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+#showing how data can be added to a html page
+posts = [
+    {
+        'route':'9',
+        'from' :'Charlestown',
+        'to':'Limekiln Avenue',
+        'time':'10:40'
+    },
+    {
+        'route':'39',
+        'from' :'Burlington Road',
+        'to':'Ongar',
+        'time':'07:00'
+    }
+]
+
+
 
 # create home function to handle traffic from journeyplanner app
 # return what we want the user to see when they are sent to this route
 
 # request arg has to be here
 def home(request):
-    return HttpResponse('<h1>HomePage!</h1>')
+    #context is a dictionary. the keys will be accessible from within the home.html template
+    context = {
+        'posts': posts
+    }
+    # context is given as argument. This passes that data into the home template
+    # posts variable will now be accessible from within home.html
+    return render(request, 'journeyplanner/home.html', context) #render still returns a HttpResponse
 
 def about(request):
-    return HttpResponse('<h1>About Page!</h1>')
+    # can also pass dictionary in directly as arg
+    return render(request, 'journeyplanner/about.html', {'title': 'About'})
 
 # Create your views here.
 
