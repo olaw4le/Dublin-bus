24b0a904ee2628b3bb350ff3b46e6c5cae755816
Laura Dunne <laura@lauradunne.ie>
2020-06-24 19:11:54 +0100

    changes to getting-termini.py to make it run for a series of files and output to .csv. For ten files it's throwing an error.

--- data-analytics/getting-termini.py
+++ data-analytics/getting-termini.py
@@ -1,36 +1,17 @@
 import pandas as pd
 import numpy as np
+import csv
+import os
+
+
 
 import warnings
 warnings.filterwarnings('ignore')
 
-df = pd.read_csv("~/Desktop/Trimester_3/FortyFour_Master.csv", keep_default_na=True, sep=',\s+', delimiter=',', skipinitialspace=True)
-
-df['DAYOFSERVICE'] = df['DAYOFSERVICE'].astype('datetime64[ns]')
-df['TRIPID'] = df['TRIPID'].astype('category')
-df['PROGRNUMBER'] = df['PROGRNUMBER'].astype('category')
-df['STOPPOINTID'] = df['STOPPOINTID'].astype('category')
-df['LINEID'] = df['LINEID'].astype('category')
-df['DIRECTION'] = df['DIRECTION'].astype('category')
-df['ROUTEID'] = df['ROUTEID'].astype('category')
-df['VEHICLEID'] = df['VEHICLEID'].astype('category')
-df['UNIQUE_TRIP'] = df['UNIQUE_TRIP'].astype('category')
-df['MONTH'] = df['MONTH'].astype('category')
-df['DAYOFWEEK'] = df['DAYOFWEEK'].astype('category')
-df['ARRIVALTIME'] = df['ARRIVALTIME'].astype('<U8')
-df['DEPARTURETIME'] = df['DEPARTURETIME'].astype('<U8')
-df['TIME_GROUP'] = df['TIME_GROUP'].astype('category')
-
-
-direction1 = df[df['DIRECTION']==1]
-direction2 = df[df['DIRECTION']==2]
-
 
 def getmainroutes(direction1, direction2):
     """returns a list of the main routes"""
-    #separating dataframe into directions
     
-
     #list of the two directions
     directions = [direction1, direction2]
     main_routes = []
@@ -44,25 +25,44 @@ def getmainroutes(direction1, direction2):
         z = y.index.values.tolist()
         
         # list index 0 is the most popular routeID in this direction
-        main_routes.append(z[0])
+        main_routes.append(z[0])   
     return main_routes
 
+
+def returnroute(direction1, direction2):
+    """returns a list of the line ids - expected to be the same for both directions"""
+    directions = [direction1, direction2]
+    line_id = []
+    #iterate through the list of directions
+    for direction in directions: 
+        #get a series of only the lineIDS and their counts  
+        fee = direction['LINEID'].value_counts()
+        #make a list of the LINEIDS
+        fi = fee.index.values.tolist()
+        #returns the first index of above (ie the most popular one, which will be the LINEID required)
+        line_id.append(fi[0])
+    return line_id
+
+
 def perfectsubset(direction1, direction2, main_routes):
+    """returns True if the less popular ROUTEIDs are perfect subsets of the main route. Otherwise returns False"""
     directions = [direction1, direction2]
 
     for direction in directions:
         z = []
         y = direction['ROUTEID'].value_counts()
         
-        z = int(len(y)/2)
+        if len(y) > 1:
+            z = int(len(y)/2)
+
         for row in y:
             if row != 0:
-                v = list(y[y != 0].index[0:z])
+                t = list(y[y != 0].index[0:z])
 
         
         not_main = []
         
-        for i in v:
+        for i in t:
             if i in main_routes:
                 main_id = i
             else: 
@@ -104,17 +104,60 @@ def firstandlaststopid(direction):
     return list_stops
 
 
-mainroutes = getmainroutes(direction1, direction2)
-main_dir1 = direction1[direction1['ROUTEID']==mainroutes[0]]
-main_dir2 = direction2[direction2['ROUTEID']==mainroutes[1]]
+#def driver_code(a):
+    #"""driver code and writes to .csv file"""
+count = 0
+directory = '/Users/laura/Desktop/Master-Route-Files'
+for entry in os.scandir(directory):
+    if entry.path.endswith(".csv"):
+        df = pd.read_csv(entry, keep_default_na=True, sep=',\s+', delimiter=',', skipinitialspace=True)
 
-y = perfectsubset(direction1, direction2, mainroutes)
-if y:
-    print("all subroutes are perfect subsets")
-else:
-    print("some subroutes are not perfect subsets")
+#df = pd.read_csv("/Users/laura/Desktop/FortyFour_Master.csv", keep_default_na=True, sep=',\s+', delimiter=',', skipinitialspace=True)
+        df['DAYOFSERVICE'] = df['DAYOFSERVICE'].astype('datetime64[ns]')
+        df['TRIPID'] = df['TRIPID'].astype('category')
+        df['PROGRNUMBER'] = df['PROGRNUMBER'].astype('category')
+        df['STOPPOINTID'] = df['STOPPOINTID'].astype('category')
+        df['LINEID'] = df['LINEID'].astype('category')
+        df['DIRECTION'] = df['DIRECTION'].astype('category')
+        df['ROUTEID'] = df['ROUTEID'].astype('category')
+        df['VEHICLEID'] = df['VEHICLEID'].astype('category')
+        df['UNIQUE_TRIP'] = df['UNIQUE_TRIP'].astype('category')
+        df['MONTH'] = df['MONTH'].astype('category')
+        df['DAYOFWEEK'] = df['DAYOFWEEK'].astype('category')
+        df['ARRIVALTIME'] = df['ARRIVALTIME'].astype('<U8')
+        df['DEPARTURETIME'] = df['DEPARTURETIME'].astype('<U8')
+        df['TIME_GROUP'] = df['TIME_GROUP'].astype('category')
 
-v = firstandlaststopid(main_dir1)
-w = firstandlaststopid(main_dir2)
-combo = list(v + w)
-print (list(set(combo)))
+
+        direction1 = df[df['DIRECTION']==1]
+        direction2 = df[df['DIRECTION']==2]
+            
+        lineid = returnroute(direction1, direction2)
+        print(lineid)
+
+        mainroutes = getmainroutes(direction1, direction2)
+
+        main_dir1 = direction1[direction1['ROUTEID']==mainroutes[0]]
+        main_dir2 = direction2[direction2['ROUTEID']==mainroutes[1]]
+
+
+        y = perfectsubset(direction1, direction2, mainroutes)
+
+        v = firstandlaststopid(main_dir1)
+        w = firstandlaststopid(main_dir2)
+            
+
+        with open('start_end_stops.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            #uncomment below if you need a new header
+            #writer.writerow(["LINEID", "MAINROUTE", "PERFECT_SUBROUTE", "DIRECTION", "START_STOP", "END_STOP"])
+            writer.writerow([lineid[0], mainroutes[0], y, 1, v[0], v[1]])
+            writer.writerow([lineid[1], mainroutes[1], y, 2, w[0], w[1]])
+            print(count)
+            count += 1
+        
+#if __name__ == "__main__":
+    #a = sys.argv[1]
+
+    #driver_code(a)
+
