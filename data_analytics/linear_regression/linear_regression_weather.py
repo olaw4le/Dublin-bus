3793f2e7c9a7bd18094b57c734711cdb55cc1cb0
MBashford <54007477+MBashford@users.noreply.github.com>
2020-07-09 15:24:43 +0100

    update linear_regression_weather.py: new function get_weather_from_db() for retrieving current weather data from database

--- data_analytics/linear_regression/linear_regression_weather.py
+++ data_analytics/linear_regression/linear_regression_weather.py
@@ -3,13 +3,48 @@ import pickle
 import requests, json
 import urllib.request
 from datetime import datetime
+import db_interface as db
+import os
+from dotenv import load_dotenv
 #from sklearn.linear_model import LinearRegression
+
+# load environment variables !!
+load_dotenv()
 
 # ignore warnings
 import warnings
 warnings.filterwarnings('ignore')
 route = "84A"
 direction = 1
+
+
+def get_weather_from_db():
+    """
+    returns a tuple of the 'current' weather data from out postgres database.
+    returned values:
+    date: datetime (may return a tuple of ints)
+    timezone offset: int
+    temp: float
+    feels_like (temp): float
+    wind_speed: float
+    wind_deg (direction): int (in range 0:360)
+    weather_main: string
+    weather_description: string
+    rain_1h: float
+    """
+
+    # load environment
+    database = os.getenv("database")
+    user = os.getenv("user")
+    password = os.getenv("password")
+    host = os.getenv("host")
+    port = os.getenv("port")
+
+    sql = db.construct_sql(table_name="weather_data_current", query_type="select_all")
+    data = db.execute_sql(sql, database, user, password, host, port, retrieving_data=True)
+
+    return data
+
 
 def generate_preditction(route, startstop, endstop, date, time, direction):
     #what do I want to do....
