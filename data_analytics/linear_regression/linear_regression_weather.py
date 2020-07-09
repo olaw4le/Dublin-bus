4f695f8334edf822d705c1660ab4e2b19ebf54ea
Laura Dunne <laura@lauradunne.ie>
2020-07-09 17:22:48 +0100

    progress on linear regression

--- data_analytics/linear_regression/linear_regression_weather.py
+++ data_analytics/linear_regression/linear_regression_weather.py
@@ -6,10 +6,10 @@ from datetime import datetime
 import db_interface as db
 import os
 from dotenv import load_dotenv
+import dotenv
 #from sklearn.linear_model import LinearRegression
 
 # load environment variables !!
-load_dotenv()
 
 # ignore warnings
 import warnings
@@ -34,16 +34,17 @@ def get_weather_from_db():
     """
 
     # load environment
-    database = os.getenv("database")
-    user = os.getenv("user")
-    password = os.getenv("password")
-    host = os.getenv("host")
-    port = os.getenv("port")
-
+    database="postgres"
+    user="postgres"
+    password="YZuB%F34qYSbpp7J"
+    host="group-10-dublin-bus.cu4ammu8tjpf.eu-west-1.rds.amazonaws.com"
+    port=5432
+    weather_api_key="86baa129046e5cbaeb16af074356e579"
+    
     sql = db.construct_sql(table_name="weather_data_current", query_type="select_all")
     data = db.execute_sql(sql, database, user, password, host, port, retrieving_data=True)
 
-    return data
+    return data[0]
 
 
 def generate_preditction(route, startstop, endstop, date, time, direction):
@@ -51,24 +52,20 @@ def generate_preditction(route, startstop, endstop, date, time, direction):
     #I want to get the weather(current)
     #ultimately this will be coming from a database...
     #api request
-    url = "http://api.openweathermap.org/data/2.5/weather?id=2964574&units=metric&appid=60277b654b7eab1a458f410bb214f606"
-    response = requests.get(url)
-    x = response.json()
-    print(x)
-    weather = x['weather']
-    foo = weather[0]
-    main = foo['main']
-    description = foo['description']
+    #url = "http://api.openweathermap.org/data/2.5/weather?id=2964574&units=metric&appid=60277b654b7eab1a458f410bb214f606"
+    
+    x = get_weather_from_db()
+    
+    temp = x[2]
+    feels_like = x[3]
+    main = x[6]
+    description = x[7]
+    speed = x[4]
+    #in m/s
+    deg = x[5]
+    rain = x[8]
 
-    bar = x['main']
-    temp = bar['temp']
-    feels_like = bar['feels_like']
-
-    wind = x['wind']
-    speed = wind['speed']
-    deg = wind['deg']
-
-    print (main, description, temp, feels_like, speed, deg)
+    print (main, description, temp, feels_like, speed, deg, rain)
 
     #construct a dataframe to match the linear_regression weather one.
     #test = "dataframe"
@@ -88,4 +85,5 @@ def generate_preditction(route, startstop, endstop, date, time, direction):
     # result = answer * prediciton
 
     #return result
+get_weather_from_db()
 generate_preditction(route, 1234, 2345, "2020-14-03", 12345, direction)
