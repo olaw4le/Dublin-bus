311731482be4704593ba2e497b08c1085a137a5b
hbarrett100 <55139243+hbarrett100@users.noreply.github.com>
2020-07-13 09:59:27 +0200

    get markers for each attractions showing

--- web_app/journeyplanner/static/journeyplanner/touristmap.js
+++ web_app/journeyplanner/static/journeyplanner/touristmap.js
@@ -2,24 +2,43 @@ $(document).ready(function () {
 
     // https://developers.google.com/maps/documentation/javascript/places
     var dublin = { lat: 53.3155395, lng: -6.4161858 };
+    // var touristIcons = {
+    //     restaurant: {
+    //         icon: "<img src=static/journeyplanner/icons/com.nextbus.dublin.jpg width=20 height=20>"
+    //     }
+    // }
 
-    var request = {
-        location: dublin,
-        radius: '50000',
-        type: ['supermarket']
-    };
+    // loop through checkboxes and display markers on map using data attr
+    $(".tourist-check").change(function () {
+        if (this.checked) {
+            var type = $(this).attr("data-type");
 
-    service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);
+            var request = {
+                location: dublin,
+                radius: '50000',
+                type: type
+            };
+
+            service = new google.maps.places.PlacesService(map);
+            service.nearbySearch(request, callback);
+
+        }
+    });
 
 
-});
-
-function callback(results, status) {
+    function callback(results, status) {
     console.log(status)
     if (status == google.maps.places.PlacesServiceStatus.OK) {
         for (var i = 0; i < results.length; i++) {
             createMarker(results[i]);
+            // var marker = new google.maps.Marker({
+            //     position: results[i],
+            //     icon: touristIcons[type].icon,
+            //     map:map
+
+
         }
     }
 }
+});
+
