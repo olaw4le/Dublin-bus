2f3fe6fbd72620f2d208f05a8b50d1f7d5f99baa
MBashford <54007477+MBashford@users.noreply.github.com>
2020-07-16 10:31:34 +0100

    update get_journey_proportions.py - implement function "get_proportions()" to query database for proportions of route

--- data_analytics/get_journey_proportion.py
+++ data_analytics/get_journey_proportion.py
@@ -29,7 +29,6 @@ def stops_on_route(route, main=False, direction=1):
 
     # execute sql query
     response = db.execute_sql(sql, database, user, password, host, port, retrieving_data=True)
-    print(response)
 
     if main:
         # return the 'main' sub-route in the passed direction
@@ -46,8 +45,10 @@ def stops_on_journey(start, end, seq):
 
     start_index = seq.index(start)
     end_index = seq.index(end)
+    print(start_index)
+    print(end_index)
 
-    return seq[start_index, end_index + 1]
+    return seq[start_index: end_index + 1]
 
 
 def segments_from_stops(seq):
@@ -57,26 +58,28 @@ def segments_from_stops(seq):
 
     for i, stop in enumerate(seq):
         if i != 0:
-            segments.append("s%d-%d" % (seq[i - 1], seq[i]))
+            segments.append("s%d_%d" % (seq[i - 1], seq[i]))
 
     return segments
 
 
 def get_proportions(route, direction, segments, month, day, time):
 
-    sql = "SELECT %s FROM %s WHERE Month = %d AND Weekday = %d AND TimeGroup = %d"
+    # create sql query
     table_name = "route_%s_%s_proportions" % (str(route), str(direction))
-    attrs = ""
-    for s in segments:
-        attrs += "%s + "
-    attrs = attrs[:-2]
-    sql = sql % (table_name, attrs, month, day, time)
+    sql = db.construct_sql(table_name=table_name, query_type="select_where",
+                           data={"month": month, "weekday": day, "timegroup": str(time)},
+                           column_names=segments)
 
     # execute sql query
     response = db.execute_sql(sql, database, user, password, host, port, retrieving_data=True)
 
-    return response
+    # return the sum of all proportions
+    return sum(response[0])
 
 
-print(stops_on_route(270, main=True, direction=1))
-#print(get_proportions(270, 2, segments_from_stops(stops_on_journey(3352, 3337, stops_on_route(270, main=True, direction=1))), 2, 2, 15))
+# ---- EXAMPLE USAGE ----
+# all_stops = stops_on_route("270", main=True, direction=2)
+# sub_stops = stops_on_journey(3333, 7026, all_stops)
+# sub_segments = segments_from_stops(sub_stops)
+# print(get_proportions(270, 2, sub_segments, "January", "Monday", 15))
