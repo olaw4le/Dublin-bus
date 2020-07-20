31d2ff0536ddaf3e033c1fc7dd28c64ed0aaf173
Laura Dunne <laura@lauradunne.ie>
2020-07-20 14:39:33 +0100

    messing around with db

--- web_app/data_analytics/linear_regression_weather.py
+++ web_app/data_analytics/linear_regression_weather.py
@@ -5,7 +5,7 @@ import math
 import requests
 import urllib.request
 from datetime import datetime
-#import db_interface as db
+import db_interface as db
 import os
 # from dotenv import load_dotenv
 # import dotenv
@@ -16,108 +16,9 @@ import warnings
 
 warnings.filterwarnings('ignore')
 
-# import psycopg2 as ps
-import pg8000 as ps
+#import psycopg2 as ps
+#import pg8000 as ps
 
-
-def construct_sql(**kwargs):
-    if "table_name" in kwargs:
-        table_name = kwargs["table_name"]
-    else:
-        print("Error: No table name supplied")
-        return False
-
-    if "query_type" in kwargs:
-        query_type = kwargs["query_type"]
-    else:
-        print("Error: No query type supplied")
-        return False
-
-    # define template structures for sql queries
-    templates = {
-        "delete_all": "DELETE FROM %s",
-        "select_all": "SELECT * FROM %s",
-        "insert": "INSERT INTO %s (%s) VALUES (%s)",
-        "attr_names": """SELECT column_name FROM information_schema.columns WHERE table_name = '%s' ORDER BY ordinal_position"""
-    }
-
-    # if inserting data
-    if query_type == "insert":
-
-        # check that data passed as kwarg
-        if "data" not in kwargs:
-            print("Error: No data supplied for insert query")
-            return False
-
-        attr_names = ""
-        attr_values = ""
-
-        # for item in data to be inserted;
-        # build a string containing the attribute names and attributes values
-        for key in kwargs["data"].keys():
-
-            attr_names += "%s, " % key
-            val = kwargs["data"][key]
-
-            # place string values in single quotes
-            if type(val) is str:
-                val = "'%s'" % val
-            else:
-                val = str(val)
-
-            attr_values += "%s, " % val
-
-        # remove ", " from the end of attr_names & attr_Values
-        attr_names = attr_names[:-2]
-        attr_values = attr_values[:-2]
-
-        # combine the query template, table name, attribute names & attribute values
-        sql_query = templates[query_type] % (table_name, attr_names, attr_values)
-
-        return sql_query
-
-    # if deleting/selecting *all* data from a table
-    elif query_type in ["delete_all", "select_all", "attr_names"]:
-        sql_query = templates[query_type] % table_name
-        return sql_query
-
-    else:
-        print("Error: Unsupported query type entered")
-        return False
-
-
-def execute_sql(sql_query, database, user, password, host, port, **kwargs):
-    # is function expected to produce output or not - depends on query type
-    if ("retrieving_data" in kwargs) and type(kwargs["retrieving_data"] == bool):
-        retrieving_data = kwargs["retrieving_data"]
-    else:
-        retrieving_data = False
-
-    # try to establish a connection to the database
-    try:
-        connection = ps.connect(database=database, user=user, password=password, host=host, port=port)
-    except Exception as e:
-        return e
-
-    cursor = connection.cursor()
-
-    # try to execute the sql query
-    try:
-        cursor.execute(sql_query)
-        connection.commit()
-    except Exception as e:
-        connection.close()
-        print(e)
-        return e
-
-    # if retrieving data / expecting some kind of response...
-    if retrieving_data:
-        response = cursor.fetchall()
-        connection.close()
-        return response
-
-    else:
-        connection.close()
 
 
 def get_weather_from_db():
@@ -131,10 +32,10 @@ def get_weather_from_db():
     port = 5432
     weather_api_key = "86baa129046e5cbaeb16af074356e579"
 
-    sql = construct_sql(table_name="weather_data_current", query_type="select_all")
-    data = execute_sql(sql, database, user, password, host, port, retrieving_data=True)
-
-    return data[0]
+    sql = db.construct_sql(table_name="weather_data_current", query_type="select_all")
+    data = db.execute_sql(sql, database, user, password, host, port, retrieving_data=True)
+    print (data)
+    return data #[0]
 
 
 def time_group_function(row):
@@ -514,4 +415,4 @@ def generate_prediction(route, startstop, endstop, date, time, direction):
     return minutes
 
 
-#generate_prediction("220", 4687, 1827, "2020-02-03", 36500, 1)
+generate_prediction("220", 4687, 1827, "2020-02-03", 36500, 1)
