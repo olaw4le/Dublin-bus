e25bf5b92bc59034189aae189e33ed55e25d4a2f
MBashford <54007477+MBashford@users.noreply.github.com>
2020-07-22 15:22:27 +0100

    update leapcard.js - implement function displayOverview() for writing leapcard details to html

--- web_app/journeyplanner/static/journeyplanner/leapcard.js
+++ web_app/journeyplanner/static/journeyplanner/leapcard.js
@@ -1,25 +1,40 @@
+/*
 $(document).ready(function () {
 
 
 });
-
+*/
 
 // login to leap api & return card overview
 function leap_login(){
 
     // get the entred username & password
-    var this_user = $("leap-user").val()
-    var this_passwd = $("leap-password").val()
+    var this_user = $("#leap-user").val();
+    var this_passwd = $("#leap-password").val();
 
     // request to get the bus time table, this should be done in the django app
     $.ajax({
         type:"POST",
         url:"leap_login/",
         data:{user:this_user, passwd:this_passwd}
-      })
+    })
 
-      .done(function(response){
-          var x = JSON.parse(response)
-          return x }
+        .done(function(response){
+            var overviewData = JSON.parse(response);
+            console.log(overviewData);
+
+            displayOverview(overviewData);
+        })
+}
+
+
+// display the overview information on the leap.html page
+function displayOverview(overviewData) {
+
+    $("#card-name").html(overviewData.cardName);
+    var a = $("#card-balance").html(overviewData.cardBalance);
+    var a = $("#card-type").html(overviewData.cardType);
+    var a = $("#card-number").html(overviewData.cardNumber);
+    var a = $("#card-expiry").html(overviewData.cardExpiry);
 
 }
