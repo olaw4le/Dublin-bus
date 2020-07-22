fd37eb4e844d9cf0b146d00168e19f11ba59b364
hbarrett100 <55139243+hbarrett100@users.noreply.github.com>
2020-07-22 15:56:15 +0200

    fix leap and realtime height bug

--- web_app/journeyplanner/static/journeyplanner/leapcard.js
+++ web_app/journeyplanner/static/journeyplanner/leapcard.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
 
+    $('#leap-login-button').on('click', function () {
 
+        console.log("inside leap click");
+        $('#leap-login-container').hide();
+        $('#leap-overview-container').show();
+        $('#leap-detailed-container').show();
+    })
 });
