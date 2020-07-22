db0d1423c24781063c427f0c328be66d53a1b7b6
MBashford <54007477+MBashford@users.noreply.github.com>
2020-07-22 12:36:53 +0100

    update leapcard.js - implement function leap_login() for returning leap card overview to user

--- web_app/journeyplanner/static/journeyplanner/leapcard.js
+++ web_app/journeyplanner/static/journeyplanner/leapcard.js
@@ -2,3 +2,24 @@ $(document).ready(function () {
 
 
 });
+
+
+// login to leap api & return card overview
+function leap_login(){
+
+    // get the entred username & password
+    var this_user = $("leap-user").val()
+    var this_passwd = $("leap-password").val()
+
+    // request to get the bus time table, this should be done in the django app
+    $.ajax({
+        type:"POST",
+        url:"leap_login/",
+        data:{user:this_user, passwd:this_passwd}
+      })
+
+      .done(function(response){
+          var x = JSON.parse(response)
+          return x }
+
+}
