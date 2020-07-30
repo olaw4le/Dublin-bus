6e764a92ea3a265d7251a4aa885d0d912c8eb660
MBashford <54007477+MBashford@users.noreply.github.com>
2020-07-30 16:12:25 +0100

    first_commit paths_to_db.py - script for inserting shapes data onto the database - workaround for issues using datagrip to read directly from .csv

---
+++ web_app/data_analytics/paths_to_db.py
@@ -0,0 +1,22 @@
+from db_interface import *
+import csv
+
+# hard code env variables to appease laura - major security vulnerability !
+database = "postgres"
+user = "postgres"
+password = "YZuB%F34qYSbpp7J"
+host = "group-10-dublin-bus.cu4ammu8tjpf.eu-west-1.rds.amazonaws.com"
+port = 5432
+
+file_path = "route_shapes.csv"
+
+with open(file_path, "r") as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+
+        sql = construct_sql(table_name="db_gtfs_shapes", query_type="insert", data=dict(row))
+        print(sql)
+
+        # execute sql query
+        print(execute_sql(sql, database, user, password, host, port))
+
