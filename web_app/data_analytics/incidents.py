7f7a470b6c0732fa0900f6156eeb7bb714ff2dab
Laura Dunne <laura@lauradunne.ie>
2020-08-11 13:14:16 +0100

    initial attempt at traffic incidents

--- web_app/data_analytics/incidents.py
+++ web_app/data_analytics/incidents.py
@@ -1,6 +1,22 @@
 # from data_analytics import db_interface as db
 import db_interface.db_interface as db
 
-def return_incident_info():
 
-    return [(53.39756, -6.26638), (53.39678, -6.26423), 'Closed between Balbutcher Lane and R108 - Closed. ']
+# load environment
+database = "postgres"
+user = "postgres"
+password = "YZuB%F34qYSbpp7J"
+host = "group-10-dublin-bus.cu4ammu8tjpf.eu-west-1.rds.amazonaws.com"
+port = 5432
+
+def return_incident_info(route, direction, date, time, start_stop_x, start_stop_y, end_stop_x, end_stop_y):
+    print(route)
+    print(direction)
+    print(date)
+    print(time)
+    route_string = str(route.lower()) + "_" + str(direction)
+    print(route_string)
+    sql = "SELECT * FROM incident_lookup l, incident_data d WHERE l.route_id = %s AND l.incident_id = d.incident_id;" % (route_string)
+    x = db.execute_sql(sql, database, user, password, host, port)
+    
+    return [(start_stop_x, start_stop_y), (end_stop_x, end_stop_y), 'Closed between Balbutcher Lane and R108 - Closed. ']
